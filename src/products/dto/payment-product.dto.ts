7ed096da4ed892de8adcf9d121acc5df1481d84c
hefi1413 <hefi1413@gmail.com>
2022-10-04 15:39:03 -0300

    update: correções DTO
    - alterações no payments-products-dto.ts para conformidade com paylod de request

--- src/products/dto/payment-product.dto.ts
+++ src/products/dto/payment-product.dto.ts
@@ -8,7 +8,7 @@ export class ProducPaymenttDto {
   @IsObject()
   @ApiProperty({
     description: 'Detalhes do produo',
-    example: '{ "cod":"xxxxxxxxx-xxxxxxxxx-0", "name": "microsystems 150 Panasonic", "value": 1200.00 }'
+    example: '{ "codigo":"xxxxxxxxx-xxxxxxxxx-0", "nome": "microsystems 150 Panasonic", "valor": 1200.00 }'
   })
   produto: Product;  
 
@@ -16,7 +16,7 @@ export class ProducPaymenttDto {
   @IsObject()
   @ApiProperty({
     description: 'Detalhes do pagamento',
-    example: '{ "entry": 500.00, "amount": 6 }'
+    example: '{ "valorEntrada": 500.00, "qtdeParcelas": 6 }'
   })
   condicaoPagamento: Payments;  
 
